0a9d42c25ef0a5540f4c34dfe9b614b0287342c0
Ali-imran-developer <ai8440018@gmail.com>
2025-07-23 03:13:11 +0500

    frontend firebase configuration

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// Firebase configuration
+const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const FIREBASE_PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = import.meta.env.VITE_FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4vRihjkpNzH4ziw5FBVS9Gne6zjderJg",
-  authDomain: "imagetotext-88a73.firebaseapp.com",
-  projectId: "imagetotext-88a73",
-  storageBucket: "imagetotext-88a73.firebasestorage.app",
-  messagingSenderId: "40708713979",
-  appId: "1:40708713979:web:566f4eb6b14ccdc7e76f5e",
-  measurementId: "G-DBMP0PSKZH",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
